65ee4a0a5a8a066516c235ee78f7d2a718938e5f
Nick <nicholasvnadegriff@gmail.com>
2021-10-04 13:15:18 -0400

    init

---
+++ src/shitcoin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShitcoinConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shitcoin'
---
+++ src/shitcoin/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('your shitcoin is gone')
+
+
+# Create your views here.
